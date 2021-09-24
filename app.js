0e6feda18670ab447716411930a89202d1e54e9f
Mariano Suarez <marianosuarez@hotmail.com>
2021-09-23 23:56:32 -0300

    correcion de puerto para Heroku

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require("path");
 const publicFolderPath = path.resolve(__dirname,"./public");
 app.use(express.static(publicFolderPath));
 
-app.listen(3000,() => {
+app.listen(process.env.PORT || 3000,() => {
     console.log("Servidor ok")
 });
 
